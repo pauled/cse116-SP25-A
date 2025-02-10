c98af3f2cf685d82cf185ced5e63949d164385a7
pauled <drpauled@gmail.com>
2025-02-10 11:51:46 -0500

    week 4 day 1

---
+++ src/week4/Player.java
@@ -0,0 +1,35 @@
+package week4;
+
+public class Player {
+    private int maxHP;
+    private int HP;
+    private int damageDealt;
+
+    //constructor
+    public Player(int maxHP){
+        this.maxHP=maxHP;
+        this.HP=maxHP;
+        this.damageDealt=4;
+    }
+    public int getHP(){
+        return this.HP;
+    }
+    public void takeDamage(int damage){
+        this.HP-=damage;
+    }
+    public String toString(){
+        String out="Health: "+this.getHP()+"/";
+        out+=this.maxHP;
+        return out;
+    }
+
+    public static void main(String[] args) {
+        Player p1=new Player(10);
+        Player p2=new Player(9);
+        Player p3=p1;
+        int temp=p1.getHP();
+        System.out.println(temp);
+        p1.takeDamage(5);
+        System.out.println(p1);
+    }
+}
